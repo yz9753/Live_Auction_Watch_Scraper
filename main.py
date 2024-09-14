bc61cc540f3121965ddffae2005e95ac3f618396
Molly Zhang <mollyzhang@10-21-229-194.dynapool.wireless.nyu.edu>
2024-09-13 22:04:46 -0400

    Initial project setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests 
+from bs4 import BeautifulSoup
+import random 
+
+def scrap_the_facts(url,num_facts=5): 
+    print('facts')
+    
+def main(): 
+    url = ""
+    
+    facts = scrap_the_facts(url)
+    
+#boilerplate to run the main() function 
+if __name__ == "__main__": 
+    main() 
