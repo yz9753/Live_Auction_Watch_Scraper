5fbb1a15cb489fd76eea53bcb3da3a32a8be4e5c
Ian Chang <personal.ianchang@gmail.com>
2024-09-14 12:24:08 -0400

    Ian joined the repo

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import random 
 
-def scrap_the_facts(url,num_facts=5): 
-    print('facts')
+print("Ian has joined")
+
+# def scrap_the_facts(url,num_facts=5): 
+#     print('facts')
     
-def main(): 
-    url = ""
+# def main(): 
+#     url = ""
     
-    facts = scrap_the_facts(url)
+#     facts = scrap_the_facts(url)
     
 #boilerplate to run the main() function 
-if __name__ == "__main__": 
-    main() 
+# if __name__ == "__main__": 
+#     main() 
